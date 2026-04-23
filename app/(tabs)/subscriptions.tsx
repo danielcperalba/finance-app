db87a5ad93bdccc286b8b2301692d8f55a645443
Daniel Campos <81268953+danielcperalba@users.noreply.github.com>
2026-04-23 13:22:34 -0300

    feat: implement authentication screens and layout for user sign-in and sign-up

---
+++ app/(tabs)/subscriptions.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Subscriptions = () => {
+    return (
+        <View>
+            <Text>Subscriptions</Text>
+        </View>
+    )
+}
+export default Subscriptions
